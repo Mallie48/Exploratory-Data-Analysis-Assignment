de6a76e9c87f2dd1782bca0f1f533254d377d34f
Mallie48 <mallie.sh@gmail.com>
2017-05-24 10:55:02 -0600

    Create Plot5.R

---
+++ Plot5.R
@@ -0,0 +1,9 @@
+
+> NEI$year <- factor(NEI$year, levels=c('1999', '2002', '2005', '2008'))
+> MD.onroad <- subset(NEI, fips == 24510 & type == 'ON-ROAD')
+> MD.df <- aggregate(MD.onroad[, 'Emissions'], by=list(MD.onroad$year), sum)
+> colnames(MD.df) <- c('year', 'Emissions')
+> ggplot(data=MD.df, aes(x=year, y=Emissions)) + geom_bar(stat="identity", aes(fill=year)) + guides(fill=F) + 
++     ggtitle('Total Emissions of Motor Vehicle Sources in Baltimore City, Maryland') + 
++     ylab(expression('PM'[2.5])) + xlab('Year') + theme(legend.position='none') + 
++     geom_text(aes(label=round(Emissions,0), size=1, hjust=0.5, vjust=2))
